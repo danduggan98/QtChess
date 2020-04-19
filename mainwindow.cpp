10e1e92dda59d9f2dd1aac39510999c3fcb71131
danduggan98 <dadu3890@colorado.edu>
2020-04-19 16:01:58 -0600

    Created scene and view for board

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,15 +1,27 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include "board.h"
+
+#include <QDebug>
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    //Create a scene for the board and attach it to our ui
+    QGraphicsView *board_graphics = ui->Board;
+    board_scene = new QGraphicsScene;
+    board_graphics->setScene(board_scene);
+
+    //Create the board
+    Board board(board_scene);
+    this->board_ptr = &board;
 }
 
-MainWindow::~MainWindow()
-{
+MainWindow::~MainWindow() {
     delete ui;
 }
 
--- mainwindow.h
+++ mainwindow.h
@@ -1,21 +1,26 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
+#include "board.h"
+
 #include <QMainWindow>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
 
-class MainWindow : public QMainWindow
-{
+class MainWindow : public QMainWindow {
     Q_OBJECT
 
-public:
-    MainWindow(QWidget *parent = nullptr);
-    ~MainWindow();
+    public:
+        MainWindow(QWidget *parent = nullptr);
+        ~MainWindow();
 
-private:
-    Ui::MainWindow *ui;
+    private:
+        Board* board_ptr;
+
+        Ui::MainWindow *ui;
+        QGraphicsScene *board_scene;
+
 };
 #endif // MAINWINDOW_H
