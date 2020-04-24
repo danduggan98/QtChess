109f54854ec9316d2413f322d1bf1ce695dd54b9
danduggan98 <dadu3890@colorado.edu>
2020-04-24 14:03:34 -0600

    Overloaded equality operator

--- coord.h
+++ coord.h
@@ -11,8 +11,13 @@ class Coord {
             x_ = x;
             y_ = y;
         }
+
         int X() { return x_; };
         int Y() { return y_; };
+
+        friend bool operator==(Coord& first, Coord& second) {
+            return (first.X() == second.X() && first.Y() == second.Y());
+        }
 };
 
 #endif // COORD_H
