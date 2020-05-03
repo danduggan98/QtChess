5f009459951ab5bce8d3f9be8730b2176523a643
danduggan98 <dadu3890@colorado.edu>
2020-05-02 20:57:59 -0600

    Darker square when king in check

--- square.cpp
+++ square.cpp
@@ -84,7 +84,7 @@ void Square::paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QW
     //Color the square red if contains a king in check
     if (this->get_piece() && this->get_piece()->IsUnderAttack()) {
         painter->setBrush(QBrush(QColor(200,0,0)));
-        painter->setOpacity(0.5);
+        painter->setOpacity(0.75);
         painter->drawRect(QRect(this->x_val_ * this->width_, this->y_val_ * this->height_, this->width_, this->height_));
         painter->setBrush(b);
     }
